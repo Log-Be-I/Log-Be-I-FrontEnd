5061b73c866806ca02fb16304a0b9f2b0ce7ff29
YeoSeungA <silo1116@naver.cm>
2025-04-17 12:43:44 +0900

    Header 크기 보완

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
 
         {/* 오른쪽 메뉴 아이콘 (누르면 onMenuPress 실행) */}
         <Pressable onPress={openSidebar} style={styles.menuIconContainer}>
-          <MenuIcon width={21} height={16} />
+          <MenuIcon width={26} height={21} />
         </Pressable>
       </View>
 
@@ -37,8 +37,8 @@ const styles = StyleSheet.create({
   headerContainer: {
     width: "100%",
     paddingHorizontal: 20, // 좌우 여백
-    paddingTop: 20, // 위 여백
-    paddingBottom: 12, // 아래 여백
+    paddingTop: 60, // 위 여백
+    //paddingBottom: 6, // 아래 여백
     flexDirection: "row", // 가로로 정렬
     justifyContent: "space-between", // 좌/우 끝으로 정렬
     alignItems: "center", // 세로 중앙 정렬
